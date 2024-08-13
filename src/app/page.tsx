1ba17d38303111fbbf38b67dc3199a7b86ed2869
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-08-13 15:21:05 +0200

    Main page first scaffold with mock data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/06b74556-5f75-4996-a097-d34b0f31105f-qepajh.png",
+  "https://utfs.io/f/9469487c-32de-458d-b3f5-54feef008e2f-tdx6zv.png",
+  "https://utfs.io/f/dd5f4de5-cac6-4460-8c1d-88dd8d6354df-6of150.png",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
