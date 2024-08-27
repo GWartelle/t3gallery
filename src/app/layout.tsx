327700a483ce5464298cb38abf6194dcfa6447a4
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-08-27 20:39:30 +0200

    Start implementing parallel routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{ children: React.ReactNode }>) {
+  modal,
+}: {
+  children: React.ReactNode;
+  modal: React.ReactNode;
+}) {
   return (
     <ClerkProvider>
       <html lang="en" className={`${GeistSans.variable}`}>
@@ -34,6 +38,8 @@ export default function RootLayout({
         <body className="flex flex-col gap-4">
           <TopNav />
           {children}
+          {modal}
+          <div id="modal-root" />
         </body>
       </html>
     </ClerkProvider>
