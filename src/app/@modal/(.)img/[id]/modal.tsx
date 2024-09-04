4e57c6ad2b9bb980c328b19f462a041cf5fb4fbe
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-09-04 23:26:18 +0200

    Finalize modal and parallel image routes

--- src/app/@modal/(.)img/[id]/modal.tsx
+++ src/app/@modal/(.)img/[id]/modal.tsx
@@ -21,7 +21,7 @@ export function Modal({ children }: { children: React.ReactNode }) {
   return createPortal(
     <dialog
       ref={dialogRef}
-      className="m-0 h-screen w-screen bg-zinc-900/50 text-white"
+      className="m-0 h-screen w-screen bg-black/90 text-white"
       onClose={onDismiss}
     >
       {children}
--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,7 +1,10 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
+
+  const uploaderInfo = await clerkClient.users.getUser(image.userId);
 
   return (
     <div className="flex h-full w-full min-w-0">
@@ -10,7 +13,16 @@ export default async function FullPageImageView(props: { id: number }) {
       </div>
 
       <div className="flex w-48 flex-shrink-0 flex-col border-l">
-        <div className="text-xl font-bold">{image.name}</div>
+        <div className="border-b p-2 text-center text-lg">{image.name}</div>
+
+        <div className="flex flex-col p-2">
+          <span>Uploaded By:</span>
+          <span>{uploaderInfo.fullName}</span>
+        </div>
+        <div className="flex flex-col p-2">
+          <span>Created On:</span>
+          <span>{new Date(image.createdAt).toLocaleDateString()}</span>
+        </div>
       </div>
     </div>
   );
