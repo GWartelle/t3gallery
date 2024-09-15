bc6b1032899a89481e328839f5dacb775de34a14
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-09-15 20:30:36 +0200

    Fixing the upload button

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,26 +1,17 @@
-"use client";
-
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import { UploadButton } from "../../utils/uploadthing";
-import { useRouter } from "next/navigation";
+import { SimpleUploadButton } from "./simple-upload-button";
 
 export function TopNav() {
-  const router = useRouter();
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Gallery</div>
 
-      <div className="flex flex-row">
+      <div className="flex flex-row items-center gap-4">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
-          <UploadButton
-            endpoint="imageUploader"
-            onClientUploadComplete={() => {
-              router.refresh();
-            }}
-          />
+          <SimpleUploadButton />
           <UserButton />
         </SignedIn>
       </div>
--- src/utils/uploadthing.ts
+++ src/utils/uploadthing.ts
@@ -1,9 +1,12 @@
 import {
-    generateUploadButton,
-    generateUploadDropzone,
-  } from "@uploadthing/react";
-   
-  import type { OurFileRouter } from "~/app/api/uploadthing/core";
-   
-  export const UploadButton = generateUploadButton<OurFileRouter>();
-  export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
+  generateReactHelpers,
+  generateUploadButton,
+  generateUploadDropzone,
+} from "@uploadthing/react";
+
+import type { OurFileRouter } from "~/app/api/uploadthing/core";
+
+export const UploadButton = generateUploadButton<OurFileRouter>();
+export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
+
+export const { useUploadThing } = generateReactHelpers<OurFileRouter>();
