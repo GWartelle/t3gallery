9b4127da874e91b0d0dad681bc6b473bf0c5303f
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-09-17 11:06:59 +0200

    Logo update

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,11 +1,14 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { SimpleUploadButton } from "./simple-upload-button";
-import Link from "next/link"
+import Link from "next/link";
 
 export function TopNav() {
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
-      <Link href="/">Gallery</Link>
+      <Link href="/" className="flex items-center gap-2">
+        <img src="/album-logo.svg" alt="Album logo" className="h-8" />
+        T3 Gallery
+      </Link>
 
       <div className="flex flex-row items-center gap-4">
         <SignedOut>
